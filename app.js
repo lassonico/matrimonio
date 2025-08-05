a37026307014cef126209c1bda9ddc73efda0bbe
lassonico <sionvmx@gmail.com>
2025-08-04 22:34:36 -0500

    actualizacion

---
+++ app.js
@@ -0,0 +1,57 @@
+console.log("On line")
+ // Simulación de base de datos de contactos
+const contactos = [
+ { telefono: "3001234567", nombre: "Carlos" },
+ { telefono: "3109876543", nombre: "Ana" },
+ { telefono: "3205551122", nombre: "Luis" }
+];
+ // Función para obtener parámetros de la URL
+function obtenerParametro(nombre) {
+ const urlParams = new URLSearchParams(window.location.search);
+ return urlParams.get(nombre);
+}
+ // Buscar el número en la lista y mostrar el nombre
+const telefono = obtenerParametro("telefono");
+const contacto = contactos.find(c => c.telefono === telefono);
+const mensaje = document.getElementById("mensaje");
+if (contacto) {
+ mensaje.textContent = `Hola ${contacto.nombre}, acompañanos en éste día y celebra con nosotros ésta nueva etapa en nuestras vidas.`;
+} else {
+ mensaje.textContent = "No se encontró tu número en la base de datos.";
+}
+
+(function(){emailjs.init("zWBQvU7UzVSMDeCkT");})();
+
+function buscarNombre(telefono) {
+    const contacto = contactos.find(c => c.telefono === telefono);
+    return contacto ? contacto.nombre : null;
+}
+  
+function enviarConfirmacion() {
+emailjs.sendForm('service_7c2hsj4', 'template_nfrrcsi', '#formulario')
+    .then(function(response) {
+    alert(`Gracias por confirmar tu asistencia ${nombre}. ¡Nos vemos en la boda!`);
+    }, function(error) {
+    alert("Hubo un error al enviar la confirmación. Intenta nuevamente.");
+    console.error(error);
+    });
+}
+
+window.addEventListener("load", function() {
+    setTimeout(function() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("contenido").style.display = "block";
+
+    const telefono = obtenerParametro("telefono");
+    const nombre = buscarNombre(telefono);
+
+    const mensaje = document.getElementById("mensaje");
+    if (nombre) {
+        mensaje.textContent =
+        `Hola ${nombre}, esta es tu invitación a nustro matrimonio.
+        `;
+    } else {
+        mensaje.textContent = "Lo sentimos no te encontramos en nuestra lista de invitados.";
+    }
+    }, 6000);
+});
